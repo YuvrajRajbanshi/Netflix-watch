b2ec1a99649e217ea9ddcc46611fc9ad35eb62ee
Yuvi <singhraj0461@gmail.com>
2024-05-13 18:03:55 +0530

    till Youtube has done

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,32 +1,56 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
 
 const TitleCards = ({ title, category }) => {
-    const cardsRef = useRef();
-    const handleWheel = (event) => {
-        event.preventDefault();
-        cardsRef.current.scrollLeft += event.deltaY;
-    };
-    useEffect(() => {
-        cardsRef.current.addEventListener("wheel", handleWheel);
-    }, []);
+  const [apiData, setApiData] = useState([]);
 
-    return (
-        <div className="titlecards">
-            <h2>{title ? title : "Popular on Netflix"} </h2>
-            <div className="card-list" ref={cardsRef}>
-                {cards_data.map((card, index) => {
-                    return (
-                        <div className="card" key={index}>
-                            <img src={card.image} alt="" />
-                            <p>{card.name}</p>
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
-    );
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWQ0OTE5YWM4MjdiMTAzNjA0YTdkMGZiNWU4NTI5ZSIsInN1YiI6IjY2NDBmN2JjNjI2YzAxNjkzZWZmNjQzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZEAkjfo0EFb-6eUxwjLQSjx6LT-XQFMsTxFWE831pM8",
+    },
+  };
+
+  const cardsRef = useRef();
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="titlecards">
+      <h2>{title ? title : "Popular on Netflix"} </h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
-
-import "./Player.css"
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useParams } from "react-router-dom";
 
 const Player = () => {
-    return (
-        <div>Player</div>
-    )
-}
+  const { id } = useParams();
 
-export default Player
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWQ0OTE5YWM4MjdiMTAzNjA0YTdkMGZiNWU4NTI5ZSIsInN1YiI6IjY2NDBmN2JjNjI2YzAxNjkzZWZmNjQzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZEAkjfo0EFb-6eUxwjLQSjx6LT-XQFMsTxFWE831pM8",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        frameborder="0"
+        title="trailer"
+        allowFullScreen
+      ></iframe>
+
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
